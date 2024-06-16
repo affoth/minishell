52be9f63a6bfe6cd037e4703303e7e864c79a624
Murat Okutucu <moktu123@gmail.com>
2024-06-16 13:52:29 +0200

    catch up

--- src/built_ins/echo.c
+++ src/built_ins/echo.c
@@ -6,14 +6,14 @@
 /*   By: mokutucu <mokutucu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 18:28:26 by mokutucu          #+#    #+#             */
-/*   Updated: 2024/06/03 18:58:44 by mokutucu         ###   ########.fr       */
+/*   Updated: 2024/06/16 13:31:23 by mokutucu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
 // Check if -n flag is present in arguments
-bool	has_suppress_newline_flag(t_arg *args_head)
+bool	suppress_n_flag(t_arg *args_head)
 {
 	t_arg *tmp = args_head;
 	while (tmp)
@@ -45,7 +45,7 @@ void	built_in_echo(t_arg *args_head)
 {
 	bool suppress_newline;
 
-	suppress_newline = has_suppress_newline_flag(args_head);
+	suppress_newline = suppress_n_flag(args_head);
 	print_echo_arguments(args_head);
 	if (!suppress_newline)
 		ft_printf("\n");
