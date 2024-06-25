e0af12790008dcf6d68204e40076f8d2a9675505
Murat Okutucu <mokutucu@c4a8c8.42berlin.de>
2024-06-25 17:09:38 +0200

    add new prompt for no input, checks for ctrl+d which return null

--- src/main.c
+++ src/main.c
@@ -6,40 +6,55 @@
 /*   By: mokutucu <mokutucu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 14:58:44 by mokutucu          #+#    #+#             */
-/*   Updated: 2024/06/25 14:52:07 by mokutucu         ###   ########.fr       */
+/*   Updated: 2024/06/25 17:08:15 by mokutucu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+//getting input and making sure that no input gives new prompt or exits (ctrl+d)
+char	*get_input()
+{
+	char	*line;
+
+	line = readline("minishell$ ");
+	 if (line == NULL)
+	{
+		printf("exit\n");
+		exit(0);
+	}
+	if (*line == '\0')
+	{
+		free(line);
+		return NULL;
+	}
+	free(line);
+	return (line);
+}
 //main minishell
 int	main()
 {
-	char	*line;
-	char	*expanded;
+	char	*input;
 	t_arg	*args_head; // Declare args_head here
 
 	signal_init();
 	while (1)
 	{
-		line = readline("minishell$ ");
-		if (!line)
-			break ;
+		input = get_input();
+		input = expand_string(input);
+		if (!input)
+			continue;
+		add_history(input);
+		args_head = tokenizer(input);
+		if (find_redirections_and_pipes(args_head))
+			handle_redirections_and_pipes(args_head);
 		else
 		{
-			add_history(line);
-			expanded = expand_string(line);
-
-			args_head = tokenizer(expanded);
-			if (find_redirections_and_pipes(args_head))
-				handle_redirections_and_pipes(args_head);
-			else
-			{
-				exec_built_ins(args_head);
-				execve_args(args_head);
-			}
-			free(line);
+			exec_built_ins(args_head);
+			execve_args(args_head);
 		}
+		free(input);
+
 	}
 	ft_gc_free();
 	rl_clear_history();
