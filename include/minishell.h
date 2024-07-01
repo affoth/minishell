cdbf6afd9ebd6a8185c9b2a88d227874602792f5
afoth <afoth@student.42berlin.de>
2024-07-01 14:38:33 +0200

    saving

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mokutucu <mokutucu@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: afoth <afoth@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 14:36:35 by mokutucu          #+#    #+#             */
-/*   Updated: 2024/06/27 22:42:13 by mokutucu         ###   ########.fr       */
+/*   Updated: 2024/07/01 14:31:06 by afoth            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,6 +66,7 @@ typedef struct s_arg
 	struct s_arg *prev;
 	struct s_arg *next;
 	int exit_status;
+	int fd[2];
 } t_arg;
 
 // Token struct
--- src/redirections_and_pipes/pipe_redirection.c
+++ src/redirections_and_pipes/pipe_redirection.c
@@ -3,14 +3,86 @@
 /*                                                        :::      ::::::::   */
 /*   pipe_redirection.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mokutucu <mokutucu@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: afoth <afoth@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 21:08:32 by afoth             #+#    #+#             */
-/*   Updated: 2024/06/27 22:37:11 by mokutucu         ###   ########.fr       */
+/*   Updated: 2024/07/01 14:31:27 by afoth            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+
+void	multipipe_redirection(t_arg *head, t_arg *tmp)
+{
+	pid_t	pid1;
+	pid_t	pid2;
+
+	if (pipe(head->fd) == -1)
+	{
+		perror("pipe");
+		return ;
+	}
+	pid1 = fork();
+	if (pid1 == -1)
+	{
+		perror("fork");
+		close(fd[0]);
+		close(fd[1]);
+		return ;
+	}
+	if (pid1 == 0)
+	{
+		if (dup2(fd[1], STDOUT_FILENO) == -1)
+		{
+			perror("dup2");
+			close(fd[0]);
+			close(fd[1]);
+			return ;
+		}
+		close(fd[0]);
+		close(fd[1]);
+		redirect_execve_args(tmp);
+		exit(0);
+	}
+	if (pid1 > 0)
+	{
+		printf("\nchild1\n");
+		printf("pid1: %d\n", pid1);
+	}
+	waitpid(pid1, NULL, 0);
+	close(fd[1]);
+	pid2 = fork();
+	if (pid2 == -1)
+	{
+		perror("fork");
+		close(fd[0]);
+		close(fd[1]);
+		return ;
+	}
+	if (pid2 == 0)
+	{
+		if (dup2(fd[0], STDIN_FILENO) == -1)
+		{
+			close(fd[0]);
+			close(fd[1]);
+			perror("dup2");
+			return ;
+		}
+		close(fd[0]);
+		close(fd[1]);
+		redirect_execve_args(head->next);
+		exit(0);
+	}
+	if (pid2 > 0)
+	{
+		printf("\nchild2\n");
+		printf("pid2: %d\n", pid2);
+	}
+	//make sure to close the pipes in the parent process
+	close(fd[0]);
+	waitpid(pid2, NULL, 0);
+}
 
 //what return type should be?
 void	pipe_redirection(t_arg *head, t_arg *tmp)
