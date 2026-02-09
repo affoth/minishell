ed202b9e90f36e7d979721fe80f14a39ac31a886
afoth <afoth@student.42berlin.de>
2026-02-09 18:41:06 +0100

    Backup

--- src/5.execution/execve_pipes.c
+++ src/5.execution/execve_pipes.c
@@ -6,11 +6,7 @@
 /*   By: afoth <afoth@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 14:16:51 by mokutucu          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2024/09/21 18:45:26 by afoth            ###   ########.fr       */
-=======
-/*   Updated: 2024/09/21 14:02:52 by afoth            ###   ########.fr       */
->>>>>>> 9133e6968902d1243bd25ca601f17eeb5578c339
+/*   Updated: 2024/12/10 22:15:13 by afoth            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +27,8 @@ int	execute_command(t_shell *shell, t_command *cmd)
 		exit(EXIT_PERMISSION_DENIED);
 	if (path == NULL || execve(path, args, shell->env) == -1)
 	{
-<<<<<<< HEAD
-=======
 		if (path && ft_strncmp(path, args[0], ft_strlen(args[0])))
 			free(path);
->>>>>>> 9133e6968902d1243bd25ca601f17eeb5578c339
 		ft_putstr_fd("minishell: ", STDERR_FILENO);
 		ft_putstr_fd(args[0], STDERR_FILENO);
 		ft_putendl_fd(": command not found", STDERR_FILENO);
